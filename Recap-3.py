1f36759640baa3c78bce057f05a81b5db7f1f8d3
singiyun <ssungd1@naver.com>
2023-05-08 19:07:16 +0900

    Recap-3.py

---
+++ Recap-3.py
@@ -0,0 +1,12 @@
+winner = 500
+
+if winner != 10:
+    print("If")
+elif winner <= 25:
+    print("elif")
+elif winner == 0:
+    print("elif 2")
+elif winner == 50:
+    print("elif 3")
+else:
+    print("Else")

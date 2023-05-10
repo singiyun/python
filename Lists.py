03470dd7a84f4a371fb2b38c899575f52847c476
singiyun <ssungd1@naver.com>
2023-05-10 19:30:18 +0900

    Lists.py

---
+++ Lists.py
@@ -0,0 +1,27 @@
+days_of_week = ["Mon","Tue","Wed","Thur","Fri"]
+days_of_week.clear()
+print(days_of_week)
+
+
+days_of_week = ["Mon","Tue","Wed","Thur","Fri"]
+print(days_of_week)
+days_of_week.reverse()
+print(days_of_week)
+
+
+days_of_week = ["Mon","Tue","Wed","Thur","Fri"]
+print(days_of_week)
+days_of_week.append("Sat")
+print(days_of_week)
+days_of_week.append("Sun")
+print(days_of_week)
+
+
+days_of_week = ["Mon","Tue","Wed","Thur","Fri"]
+print(days_of_week)
+days_of_week.remove("Fri")
+print(days_of_week)
+
+
+days_of_week = ["Mon","Tue","Wed","Thur","Fri"]
+print(days_of_week[2])

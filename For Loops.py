22bb948ee4ec11b86d9be05bcbdddb93d24ebc17
singiyun <ssungd1@naver.com>
2023-05-15 20:10:07 +0900

    For Loops.py

---
+++ For Loops.py
@@ -0,0 +1,13 @@
+websites = (
+    "google.com",
+    "airbnb.com",
+    "twitter.com",
+    "facebook.com",
+    "tiktok.com"
+)
+
+for potato in websites:
+    print("potato is equals to", potato)
+
+for websites in websites:
+    print(websites)

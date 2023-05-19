71bdaadfa9fcd2d4d1f262c2c46891d5679d7516
singiyun <ssungd1@naver.com>
2023-05-19 20:07:16 +0900

    Job Posts.py

---
+++ Job Posts.py
@@ -0,0 +1,18 @@
+from requests import get
+from bs4 import BeautifulSoup
+
+base_url = "https://weworkremotely.com/remote-jobs/search?term="
+search_term = "python"
+
+response = get(f"{base_url}{search_term}")
+if response.status_code !=200:
+    print("Can't request website")
+else:
+    soup = BeautifulSoup(response.text, "html.parser")
+    jobs = soup.find_all('section', class_="jobs")
+    for job_section in jobs:
+        job_posts = job_section.find_all('li')
+        job_posts.pop(-1)
+        for post in job_posts:
+            print(post)
+            print("////////////////////")

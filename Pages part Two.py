8aadebf9a633fc7c914c9f3eb25b72ef86aefe31
singiyun <ssungd1@naver.com>
2023-05-29 13:06:10 +0900

    Pages part Two.py

---
+++ Pages part Two.py
@@ -0,0 +1,50 @@
+from requests import get
+from bs4 import BeautifulSoup
+from extract.wwr import extract_wwr_jobs
+
+def get_page_count(keyword):
+    base_url = "https://kr.indded.com/jobs?q="
+    response = get(f"{base_url}{keyword}")
+    if response.status_code != 200:
+        print("Can't request page")
+    else:
+        soup = BeautifulSoup(response.text, "html.parser")
+        pagination = soup.find("ul", class_="pagination-list")
+        if pagination == None:
+            return 1
+        pages = pagination.find_all("li", recursive=False)
+        count = len(pages)
+        if count >= 5:
+            return 5
+        else:
+            return count
+
+def extract_indeed_jobs(keyword):
+    pages = get_page_count(keyword)
+    for page in range(pages):
+        base_url = "https://kr.indded.com/jobs?q="
+        response = get(f"{base_url}{keyword}")
+        if response.status_code != 200:
+            print("Can't request page")
+        else:
+            results = []
+            soup = BeautifulSoup(response.text, "html.parser")
+            job_list = soup.find("ul", class="jobsearch-ResultsList")
+            jobs = job_list.find_all('li', recursive=False)
+            for job in jobs:
+                zone = job.find("div", class_="mosaic-zone")
+                if zone == None:
+                    anchor = job.select_one("h2 a")
+                    title = anchor['aria-label']
+                    link = anchor['href']
+                    company = job.find("span", class_="companyName")
+                    location = job.find("div", class_="companyLocation")      
+                    job_data = {
+                        'link': f"https://kr.indeed.com{link}", 
+                        'company': company.string, 
+                        'location': location.string,
+                        'position': title
+                    }
+                    results.append(job_data)
+            for result in results:
+                print(result, "\n//////\n")

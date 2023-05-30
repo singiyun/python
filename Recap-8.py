841a13c54cc5a3c4f76cf74f0f0425bce4c91691
singiyun <ssungd1@naver.com>
2023-05-30 19:53:17 +0900

    Recap-8.py

---
+++ Recap-8.py
@@ -0,0 +1,13 @@
+from extractors.indeed import extract_indeed_jobs
+from extractors.wwr import extract_wwr_jobs
+
+keyword = input("What do you want to search for?")
+
+indeed = extract_indeed_jobs(keyword)
+wwr = extract_wwr_jobs(keyword)
+
+jobs = indeed + wwr
+
+for job in jobs:
+    print(job)
+    print("//////\n//////")
